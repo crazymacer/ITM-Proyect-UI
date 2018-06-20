1b5930f5959558cdce0f230c5ce4f5d74b328347
crazymacer <mario_ct5@hotmail.com>
2018-06-20 17:50:49 -0500

    Custom index and device home

--- dist/js/pages/dashboard.js
+++ dist/js/pages/dashboard.js
@@ -126,56 +126,82 @@ $(function () {
 
   /* Morris.js Charts */
   // Sales chart
-  var area = new Morris.Area({
-    element   : 'revenue-chart',
-    resize    : true,
-    data      : [
-      { y: '2011 Q1', item1: 2666, item2: 2666 },
-      { y: '2011 Q2', item1: 2778, item2: 2294 },
-      { y: '2011 Q3', item1: 4912, item2: 1969 },
-      { y: '2011 Q4', item1: 3767, item2: 3597 },
-      { y: '2012 Q1', item1: 6810, item2: 1914 },
-      { y: '2012 Q2', item1: 5670, item2: 4293 },
-      { y: '2012 Q3', item1: 4820, item2: 3795 },
-      { y: '2012 Q4', item1: 15073, item2: 5967 },
-      { y: '2013 Q1', item1: 10687, item2: 4460 },
-      { y: '2013 Q2', item1: 8432, item2: 5713 }
-    ],
-    xkey      : 'y',
-    ykeys     : ['item1', 'item2'],
-    labels    : ['Item 1', 'Item 2'],
-    lineColors: ['#a0d0e0', '#3c8dbc'],
-    hideHover : 'auto'
+
+  var months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+  Morris.Line({
+    element: 'revenue-chart',
+    data: [{
+      m: '2015-01', // <-- valid timestamp strings
+      a: 0
+    }, {
+      m: '2015-02',
+      a: 54
+    }, {
+      m: '2015-03',
+      a: 243
+    }, {
+      m: '2015-04',
+      a: 206
+    }, {
+      m: '2015-05',
+      a: 161
+    }, {
+      m: '2015-06',
+      a: 187
+    }, {
+      m: '2015-07',
+      a: 210,
+      b: 0
+    }, {
+      m: '2015-08',
+      a: 204
+    }, {
+      m: '2015-09',
+      a: 224
+    }, {
+      m: '2015-10',
+      a: 301
+    }, {
+      m: '2015-11',
+      a: 262
+    }, {
+      m: '2015-12',
+      a: 199
+    }, ],
+    xkey: 'm',
+    ykeys: ['a'],
+    labels: ['2014', '2015'],
+    xLabelFormat: function(x) { // <--- x.getMonth() returns valid index
+      var month = months[x.getMonth()];
+      return month;
+    },
+    dateFormat: function(x) {
+      var month = months[new Date(x).getMonth()];
+      return month;
+    },
   });
-  var line = new Morris.Line({
-    element          : 'line-chart',
-    resize           : true,
-    data             : [
-      { y: '2011 Q1', item1: 2666 },
-      { y: '2011 Q2', item1: 2778 },
-      { y: '2011 Q3', item1: 4912 },
-      { y: '2011 Q4', item1: 3767 },
-      { y: '2012 Q1', item1: 6810 },
-      { y: '2012 Q2', item1: 5670 },
-      { y: '2012 Q3', item1: 4820 },
-      { y: '2012 Q4', item1: 15073 },
-      { y: '2013 Q1', item1: 10687 },
-      { y: '2013 Q2', item1: 8432 }
-    ],
-    xkey             : 'y',
-    ykeys            : ['item1'],
-    labels           : ['Item 1'],
-    lineColors       : ['#efefef'],
-    lineWidth        : 2,
-    hideHover        : 'auto',
-    gridTextColor    : '#fff',
-    gridStrokeWidth  : 0.4,
-    pointSize        : 4,
-    pointStrokeColors: ['#efefef'],
-    gridLineColor    : '#efefef',
-    gridTextFamily   : 'Open Sans',
-    gridTextSize     : 10
-  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   // Donut Chart
   var donut = new Morris.Donut({
